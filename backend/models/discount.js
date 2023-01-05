b3ab05f0b709080874e851bd9b7226363f4743b5
Dennis <d.vd.meerendonk@live.nl>
2023-01-05 15:04:33 +0100

    a

--- backend/models/discount.js
+++ backend/models/discount.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const discountSchema =  mongoose.Schema({
   title: {type: String, required: true },
-  content: {type: String, required: true }
+  content: {type: String, required: true },
+  date: {type: Date}
 });
 
 module.exports = mongoose.model('Discount', discountSchema);
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,12 @@
+const { Int32 } = require("mongodb");
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema =  mongoose.Schema({
+  username: {type: String},
   password: {type: String, required: true },
-  email: {type: String, required: true, unique: true}
+  email: {type: String, required: true, unique: true},
+  posts: {type: Int32}
 });
 
 userSchema.plugin(uniqueValidator);
