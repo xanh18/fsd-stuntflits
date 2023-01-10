66ab369f65e21c382783cbec79a7c13886dcad08
Dennis <d.vd.meerendonk@live.nl>
2023-01-10 16:37:16 +0100

    a

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+
 import { AuthService } from "../auth.service";
 
 @Component({
@@ -9,8 +10,7 @@ import { AuthService } from "../auth.service";
 export class LoginComponent {
   isLoading = false;
 
-  constructor(public authService: AuthService) {   
-  }
+  constructor(public authService: AuthService) {}
 
   onLogin(form: NgForm) {
     if (form.invalid) {
